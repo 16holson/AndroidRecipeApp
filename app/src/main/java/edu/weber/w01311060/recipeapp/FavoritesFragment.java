49febd740e475d4f1c3069d1d92ddb4e87bb22bc
Hunter <hunterolson11@yahoo.co.uk>
2022-04-11 11:30:37 -0600

    Started favorites

--- app/src/main/java/edu/weber/w01311060/recipeapp/FavoritesFragment.java
+++ app/src/main/java/edu/weber/w01311060/recipeapp/FavoritesFragment.java
@@ -1,12 +1,21 @@
 package edu.weber.w01311060.recipeapp;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +33,7 @@ public class FavoritesFragment extends Fragment
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private View root;
 
     public FavoritesFragment()
     {
@@ -58,6 +68,7 @@ public class FavoritesFragment extends Fragment
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -65,6 +76,67 @@ public class FavoritesFragment extends Fragment
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        return root = inflater.inflate(R.layout.fragment_favorites, container, false);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        Toolbar toolbar = root.findViewById(R.id.favoritesToolbar);
+        toolbar.setTitle("Recipes");
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.recipelist, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.recipeSearch).getActionView();
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String s)
+            {
+                searchRecipes(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s)
+            {
+                searchRecipes(s);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.recipeSearch:
+
+                return true;
+            case R.id.filterRecipes:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    public void searchRecipes(String query)
+    {
+
     }
 }
